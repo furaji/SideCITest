7e22fe73762a1ddb0205e8c06538f314cca5835e
furajium <fy.sasaki@gmail.com>
2014-05-07 07:45:46 +0000

    scaffoldした

---
+++ app/views/tops/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @top, :id, :body, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SideCITest::Application.routes.draw do
+  resources :tops
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
